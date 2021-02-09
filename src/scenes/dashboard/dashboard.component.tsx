2d0c727ab5059d46aec25a2632509d08f0f5abee
boyd <boyd.coe@gmail.com>
2021-02-09 12:09:28 +0700

    add pie chart

--- src/scenes/dashboard/dashboard.component.tsx
+++ src/scenes/dashboard/dashboard.component.tsx
@@ -1,13 +1,38 @@
 import React from "react";
 import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Pie from 'react-native-pie';
 
 export class DashBoardScreen extends React.Component {
 
     render() {
+        //const sliceColor = ['#F44336','#2196F3','#FFEB3B', '#4CAF50', '#FF9800']
         return (
             <SafeAreaView style={styles.container}>
                 <Text style={styles.text}>Hellp Apollo!!</Text>
+                <Pie
+                    radius={80}
+                    sections={[
+                        {
+                        percentage: 10,
+                        color: '#C70039',
+                        },
+                        {
+                        percentage: 20,
+                        color: '#44CD40',
+                        },
+                        {
+                        percentage: 30,
+                        color: '#404FCD',
+                        },
+                        {
+                        percentage: 40,
+                        color: '#EBD22F',
+                        },
+                    ]}
+                    strokeCap={'butt'}
+                />
+
             </SafeAreaView>
         )
     } 

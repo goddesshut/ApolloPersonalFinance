3c3064f6ff9d50a18dee4f754448d589493950bc
goddesshut <hut.nuttakrti@gmail.com>
2021-02-09 18:17:10 +0700

    Add Account transaction

---
+++ src/model/categoryType.ts
@@ -0,0 +1,6 @@
+export enum CategoryType {
+    SALARY = 'Salary',
+    RENT = 'Rent',
+    Utility = 'Utility',
+    SHOPING = 'Shopping'
+}
---
+++ src/scenes/transaction/transaction-detail/transaction-detail.model.ts
@@ -0,0 +1,112 @@
+import { CategoryType } from "../../../model/categoryType";
+import { TransactionType } from "../../../model/transactionType";
+
+
+export interface AccountTransactionDetail {
+    accountNumber: string,
+    transactionType: TransactionType,
+    categoryType: CategoryType
+    price: number,
+    updateDate: string
+}
+
+export const AccountTransactionDetailData: AccountTransactionDetail[] = [
+    {
+       accountNumber: '123-221-5486-223',
+       transactionType: TransactionType.INCOME,
+       categoryType: CategoryType.SALARY,
+       price: 100000.00,
+       updateDate: '2021-01-01'
+    },
+    {
+        accountNumber: '123-221-5486-223',
+        transactionType: TransactionType.OUTCOME,
+        categoryType: CategoryType.SHOPING,
+        price: 2000.00,
+        updateDate: '2021-01-01'
+     },
+     {
+        accountNumber: '123-221-5486-223',
+        transactionType: TransactionType.OUTCOME,
+        categoryType: CategoryType.RENT,
+        price: 15000.00,
+        updateDate: '2021-01-01'
+     },
+     {
+        accountNumber: '123-221-5486-223',
+        transactionType: TransactionType.OUTCOME,
+        categoryType: CategoryType.Utility,
+        price: 5000.00,
+        updateDate: '2021-01-01'
+     },
+     {
+        accountNumber: '123-221-5486-223',
+        transactionType: TransactionType.OUTCOME,
+        categoryType: CategoryType.Utility,
+        price: 500.00,
+        updateDate: '2021-01-01'
+     },
+     {
+        accountNumber: '123-221-5486-223',
+        transactionType: TransactionType.OUTCOME,
+        categoryType: CategoryType.SHOPING,
+        price: 5987.00,
+        updateDate: '2021-01-01'
+     },
+     {
+        accountNumber: '238-453-8486-638',
+        transactionType: TransactionType.INCOME,
+        categoryType: CategoryType.SALARY,
+        price: 80000.00,
+        updateDate: '2021-01-01'
+     },
+     {
+         accountNumber: '238-453-8486-638',
+         transactionType: TransactionType.OUTCOME,
+         categoryType: CategoryType.SHOPING,
+         price: 2500.00,
+         updateDate: '2021-01-01'
+      },
+      {
+         accountNumber: '238-453-8486-638',
+         transactionType: TransactionType.OUTCOME,
+         categoryType: CategoryType.RENT,
+         price: 12000.00,
+         updateDate: '2021-01-01'
+      },
+      {
+         accountNumber: '238-453-8486-638',
+         transactionType: TransactionType.OUTCOME,
+         categoryType: CategoryType.Utility,
+         price: 3500.00,
+         updateDate: '2021-01-01'
+      },
+      {
+         accountNumber: '238-453-8486-638',
+         transactionType: TransactionType.OUTCOME,
+         categoryType: CategoryType.Utility,
+         price: 800.00,
+         updateDate: '2021-01-01'
+      },
+      {
+        accountNumber: '564-864-4233-487',
+        transactionType: TransactionType.INCOME,
+        categoryType: CategoryType.SALARY,
+        price: 1000000.00,
+        updateDate: '2021-01-01'
+     },
+     {
+        accountNumber: '124-894-0000-456',
+        transactionType: TransactionType.OUTCOME,
+        categoryType: CategoryType.SHOPING,
+        price: 10000.00,
+        updateDate: '2021-01-18'
+     },
+     {
+        accountNumber: '124-894-0000-456',
+        transactionType: TransactionType.OUTCOME,
+        categoryType: CategoryType.Utility,
+        price: 5000.00,
+        updateDate: '2020-12-28'
+     },
+]

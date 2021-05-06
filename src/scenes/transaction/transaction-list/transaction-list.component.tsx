b33d18d04a481b2874bc01166c38641867134cc5
Srasvorlak Phourng <SrasvorlakP@ATA-BKK.local>
2021-05-06 17:12:23 +0700

    [V]: api getway mock with transaction account

--- src/scenes/transaction/transaction-list/transaction-list.component.tsx
+++ src/scenes/transaction/transaction-list/transaction-list.component.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { Icon, Card } from "react-native-elements";
 import { CurrencyFormat } from "../../../utils/currency-format.component"
 import { TransactionType } from "../../../model/transactionType";
-import { TransactionDetail, TransactionDetailData } from "../model/transaction-list.model";
+import { TransactionDetail } from "../model/transaction-list.model";
 
 interface Props {
     transactionId: string;
@@ -13,11 +13,26 @@ interface Props {
 export class TransactionListScreen extends Component<Props> {
 
     private transactionDetail: TransactionDetail[] = [];
+    private accoundId = "293-92880988";
+    state = { accountTransactionDetail: [] as TransactionDetail[], transactionDetail: [] as TransactionDetail[]};
 
     constructor(props) {
         super(props);
-        // first time loading data
-        this.getTransactioDetail();
+        this.state = {
+            accountTransactionDetail: [],
+            transactionDetail:[]
+        };
+    }
+
+    componentDidMount(){
+        this.getAccountTransaction(this.accoundId).then((res) => {
+            //this.transactionDetail = res.body;
+            
+            this.setState({accountTransactionDetail: res.body});
+            this.getTransactioDetail();
+            console.log('componentDidMount', this.state.transactionDetail);
+        });
+        
     }
 
     // reload when properties has change
@@ -26,13 +41,14 @@ export class TransactionListScreen extends Component<Props> {
             this.getTransactioDetail();
             return true;
         }
-
         return false;
     }
 
     render() {
+
+        console.log('anacondong', this.state.transactionDetail);
         return (
-            this.transactionDetail.map((item, index) => (
+            this.state.transactionDetail.map((item, index) => (
                 <Card key={`tranaction-detail-${index + 1}`} containerStyle={{ borderRadius: 10 }}>
                     <View style={styles.row}>
                         <Icon reverse name='payments' size={20} color={'blue'} key={'icon' + index} />
@@ -51,11 +67,30 @@ export class TransactionListScreen extends Component<Props> {
 
     private getTransactioDetail() {
         if (this.props.type === 'account') {
-            this.transactionDetail = TransactionDetailData.filter(x => x.accountNumber === this.props.transactionId);
+            this.setState({transactionDetail : this.state.accountTransactionDetail.filter(x => x.accountNumber === this.props.transactionId)});
+            // console.log('transaction detail',this.state.transactionDetail);
+            // console.log('id', this.props.transactionId);
+            // console.log('account detail', this.state.accountTransactionDetail)
         } else {
-            this.transactionDetail = TransactionDetailData.filter(x => x.categoryType === this.props.transactionId);
+            this.setState({transactionDetail : this.state.accountTransactionDetail.filter(x => x.categoryType === this.props.transactionId)});
         }
 
+    }
+    private getAccountTransaction(accountid) {
+        return fetch('https://1to2o3kdx7.execute-api.ap-southeast-1.amazonaws.com/dev/transactions?accountid='+ accountid, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            }
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                return responseJson;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 }
 
